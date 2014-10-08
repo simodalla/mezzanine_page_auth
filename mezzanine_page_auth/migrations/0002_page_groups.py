bb3f0a5940bc915d1fa1bbb795c51ec91b8acbaa
Simone Dalla <simodalla@gmail.com>
2014-10-08 11:06:37 +0200

    page-auth

---
+++ mezzanine_page_auth/migrations/0002_page_groups.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mezzanine_page_auth', '0001_initial'),
+        ('auth', '0001_initial'),
+        ('pages', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='page',
+            name='groups',
+            field=models.ManyToManyField(blank=True, null=True, verbose_name='groups', to='auth.Group', through='mezzanine_page_auth.PageAuthGroup'),
+            preserve_default=True,
+        ),
+    ]
